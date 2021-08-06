c5cc6adc2f80f1995ec33b390afecaa19f0a695e
Y <Y@Ys-MacBook-Pro.local>
2021-08-06 17:50:30 +0700

    mentor review code

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -12,11 +12,16 @@ export default class Home extends Component {
     }
 
     getGlass = (id) => {
-        let idx = glasses.findIndex((glass) => glass.id === id)
-        if (idx != -1)
-            this.setState({
-                glass: glasses[id-1]
-            })
+        // let idx = glasses.findIndex((glass) => glass.id === id)
+        // if (idx != -1)
+        //     this.setState({
+        //         glass: glasses[id-1]
+        //     })
+
+        let glassFind = glasses.find((glass) => glass.id === id); //Nên find trực tiếp không cần find id trong trường hợp này
+        this.setState({
+            glass: glassFind
+        })
     }
 
     render() {
@@ -33,12 +38,17 @@ export default class Home extends Component {
                     </div>
                 </div>
                 <div className="glasses">
-                    {(() => {
+                    {/* {(() => {
                         let arr = [...glasses]; 
                         return arr.map((glass) => {
                             return <Glass key={glass.id} id={glass.id} getGlass={this.getGlass}/>  
                         })
-                    })()}                    
+                    })()}*/} 
+                    { //không cần quá phức tạp, khai báo let arr = [...glasses];  là vô nghĩa
+                        glasses.map(glass=>(
+                            <Glass key={glass.id + "glass"} id={glass.id} getGlass={this.getGlass}/>
+                        ))
+                    }
                 </div>
             </div>
         )
--- src/Home/Info/Info.jsx
+++ src/Home/Info/Info.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 
 export default class Info extends Component {
     render() {
+        const {glass} = this.props;
         return (
             <div className="info">
-                <img src={this.props.glass.url} alt="" />
+                <img src={glass.url} alt="" />
                 <div className="text">
-                    <h3>{this.props.glass.name}</h3>
-                    <p>{this.props.glass.desc}</p>
+                    <h3>{glass.name}</h3>
+                    <p>{glass.desc}</p>
                 </div>
             </div>
         )
